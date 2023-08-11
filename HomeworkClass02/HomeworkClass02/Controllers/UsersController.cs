4402ef5c7d5bc06cc0af74d57df9c19c7b93f403
andrearalevska <ralevska96@gmail.com>
2023-08-12 00:50:46 +0200

    Adding Homework1

---
+++ HomeworkClass02/HomeworkClass02/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeworkClass02.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> GetAll()
+        {
+            return Ok(StaticDb.Users);
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<string> GetOneUser(int index) 
+        {
+            try
+            {
+                if(index < 0)
+                {
+                   return BadRequest("The index should be a positive number");
+                }
+                if( index >=  StaticDb.Users.Count)
+                {
+                   return NotFound($"There is no user on index {index}");
+                }
+
+                return Ok(StaticDb.Users[index]);
+            }
+            catch(Exception ex) 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred");
+            }
+        }
+
+    }
+}
---
+++ HomeworkClass02/HomeworkClass02/StaticDb.cs
@@ -0,0 +1,15 @@
+﻿namespace HomeworkClass02
+{
+    public static class StaticDb
+    {
+        public static List<string> Users = new List<string>
+        {
+            "andrea123",
+            "petarralevski",
+            "kristijanch",
+            "vralevska",
+            "mmarkovska",
+            "kstefanovska"
+        };
+    }
+}
